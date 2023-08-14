00cd7416af6db0f0f0e505e9f190b69608d32f0c
Alex <alex.dvoynev2014@yandex.ru>
2023-08-14 14:59:10 +0300

    Дз №9

---
+++ Homework9/Program.cs
@@ -0,0 +1,69 @@
+﻿/*// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void NumberCounter (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write($"{number} \t");
+    NumberCounter (number - 1);
+}
+
+Console.WriteLine("Введите натуральное число больше 1:");
+int number = Convert.ToInt32(Console.ReadLine());
+NumberCounter(number);*/
+
+/*// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+int Sum(int m, int n)
+{
+    if(m == n)
+        return 0;
+    else
+    {
+        int sum = 0;
+        m++;
+        sum = m + Sum(m, n);
+        return sum;
+    }
+}
+
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write(Sum(m - 1,n));*/
+
+/*// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(Akkerman(m, n));*/
